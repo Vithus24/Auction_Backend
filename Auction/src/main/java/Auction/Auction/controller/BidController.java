1d47be25ebeae804e7612e618b79cded75f9cf40
Suvasthikan <suvasthikan20@gmail.com>
2025-09-16 20:20:39 +0530

    BidFunctionality

--- Auction/src/main/java/Auction/Auction/controller/BidController.java
+++ Auction/src/main/java/Auction/Auction/controller/BidController.java
@@ -1,9 +1,11 @@
 package Auction.Auction.controller;
 
 import Auction.Auction.entity.Bid;
+import Auction.Auction.service.AuctionService;
 import Auction.Auction.service.BidService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,10 @@ import java.util.List;
 public class BidController {
     @Autowired
     private BidService bidService;
+    @Autowired
+    private AuctionService auctionService;
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
 
     @GetMapping
     public List<Bid> getAllBids() {
@@ -29,15 +35,24 @@ public class BidController {
 
     @PostMapping
     @PreAuthorize("hasRole('TEAM_OWNER')")
-    public Bid createBid(@RequestBody Bid bid) {
-        return bidService.save(bid);
+    public ResponseEntity<Bid> createBid(@RequestBody Bid bid) {
+        // Validate and save bid using AuctionService to reuse logic
+        Bid savedBid = auctionService.placeBid(
+                bid.getPlayer().getId(),
+                bid.getTeam().getId(),
+                bid.getBidAmount()
+        );
+        return ResponseEntity.ok(savedBid);
     }
 
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('TEAM_OWNER')")
     public ResponseEntity<Bid> updateBid(@PathVariable Long id, @RequestBody Bid bid) {
         try {
-            return ResponseEntity.ok(bidService.update(id, bid));
+            Bid updatedBid = bidService.update(id, bid);
+            // Optionally broadcast updated bid
+            messagingTemplate.convertAndSend("/topic/bids", updatedBid);
+            return ResponseEntity.ok(updatedBid);
         } catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
         }
